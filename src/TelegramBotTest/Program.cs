df356c22ad8e66e077ad176c2ccc83f62edfe6d6
bahtitm <bahti1986@gmail.com>
2024-08-16 02:12:48 +0300

    swagger modify

--- src/TelegramBotTest/Program.cs
+++ src/TelegramBotTest/Program.cs
@@ -74,8 +74,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
+    
 }
+app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
