a558fade691efd1b3ffc0fd33d0e902b963348ae
bahtitm <bahti1986@gmail.com>
2024-08-18 15:54:00 +0300

    test

--- src/TelegramBotTest/Controllers/WebhookController.cs
+++ src/TelegramBotTest/Controllers/WebhookController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Features.Subscriptions.Queries.GetAll;
+using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
 using TelegramBotTest.Services;
 
@@ -18,7 +19,7 @@ namespace TelegramBotTest.Controllers
                 var inlineKeyboards = new List<List<InlineKeyboard>>
                     {
                         new List<InlineKeyboard>{
-                            new InlineKeyboard { Text = "Services", CallbackData=$"https://89.111.173.247/api/Subscriptions/forTelegram/{update.Message.Chat.Id}" },
+                            new InlineKeyboard { Text = "Services", CallbackData=$"/api/Subscriptions/forTelegram/{update.Message.Chat.Id}" },
                             new InlineKeyboard { Text = "Personal account", CallbackData = "2" }
                         }
                     };
@@ -45,6 +46,55 @@ namespace TelegramBotTest.Controllers
             logger.LogCritical(json);
             
             return NoContent();
+        }
+
+
+
+
+        [HttpGet("/api/Subscriptions/forTelegram/{chartId}")]
+        public async Task GetforTelegram(long chartId)
+        {
+            logger.LogCritical($"forTelegram/{chartId}");
+            var dataSource = await mediator.Send(new GetAllSubscriptionQuery());
+
+            var inlineKeyboards = new List<List<InlineKeyboard>>();
+            foreach (var item in dataSource)
+            {
+                var t = new List<InlineKeyboard>() { new InlineKeyboard { Text = item.Name, CallbackData = "1" } };
+                inlineKeyboards.Add(t);
+            }
+            var backButton = new List<InlineKeyboard>() { new InlineKeyboard { Text = "Назад", CallbackData = "1" } };
+            inlineKeyboards.Add(backButton);
+
+
+
+
+            var message = new MessageForSend
+            {
+                ChatId = chartId,
+                Text = "Services",
+                ReplyMarkup = new ReplyMarkup
+                {
+                    InlineKeyboard = inlineKeyboards,
+                },
+
+            };
+            var json = JsonSerializer.Serialize(message);
+            logger.LogCritical(json);
+            try
+            {
+
+                await telegramApiService.SendMessageToBot(message);
+            }
+            catch (Exception e)
+            {
+
+                logger.LogCritical(e.Message);
+
+            }
+
+
+
         }
 
 
